1b9d6502bf939c9aaf0c42f8ce6c9dd97b1addef
10666855 <Pradeep1.T@lntinfotech.com>
2021-09-16 16:49:25 +0530

    readme file changes

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
     description                           ='databalancer is the python library dedicated to balance the imbalanced text classification datasets before the model training in machine learning applications',
     version                               ='v0.0.4',
     long_description                      =README,
+    long_description_content_type         ='text/markdown',
     url                                   ='https://github.com/pradeepdev-1995/databalancer',
     packages                              =['databalancer'],
     python_requires                       =">=3.6.9",
