2d07722147a1ab9981ad4ad7001fc3dd8afc8ec4
10666855 <pradeep1.t@lntinfotech.com>
2022-07-06 18:51:00 +0530

    import changes

--- databalancer/databalancer.py
+++ databalancer/databalancer.py
@@ -1,10 +1,10 @@
-import                                                              pandas as pd
-from databalancer.paraphraseGeneratorClient import                  paraPharaseGeneratorT5
-from databalancer.paraphraseGeneratorClient import                  modelAndTokenizerInitializer
-from databalancer.paraphraseInputGeneratorClient import             paraphraseInputSentenceGenerator
-from paraphraseGeneratorUsingNlpAug import                          paraPharaseGeneratorNlpAug
-from paraphraseGeneratorUsingTextAttack import                      paraPharaseGeneratorTextAttack
-import matplotlib.pyplot                                            as plt
+import                                                 pandas as pd
+from paraphraseGeneratorClient import                  paraPharaseGeneratorT5
+from paraphraseGeneratorClient import                  modelAndTokenizerInitializer
+from paraphraseInputGeneratorClient import             paraphraseInputSentenceGenerator
+from paraphraseGeneratorUsingNlpAug import             paraPharaseGeneratorNlpAug
+from paraphraseGeneratorUsingTextAttack import         paraPharaseGeneratorTextAttack
+import matplotlib.pyplot                               as plt
 
 '''
 Datset balancer function
