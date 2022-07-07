c0f45cb87e22700ab6637916e4828ae8800e185c
10666855 <pradeep1.t@lntinfotech.com>
2022-07-07 21:40:57 +0530

    Debugging steps

--- databalancer/paraphraseGeneratorUsingNlpAug.py
+++ databalancer/paraphraseGeneratorUsingNlpAug.py
@@ -4,4 +4,6 @@ def paraPharaseGeneratorNlpAug(sentence, each_para_count, model):
     paraQuestionlist    = []
     aug                 = naw.ContextualWordEmbsAug(model_path=model, action="insert")
     paraQuestionlist    = aug.augment(sentence, n=each_para_count)
+    print("paraQuestionlist")
+    print(paraQuestionlist)
     return paraQuestionlist
--- databalancer/paraphraseGeneratorUsingTextAttack.py
+++ databalancer/paraphraseGeneratorUsingTextAttack.py
@@ -6,4 +6,6 @@ easy_aug = EasyDataAugmenter()
 def paraPharaseGeneratorTextAttack(sentence):
     paraQuestionlist                = []
     paraQuestionlist                = easy_aug.augment(sentence)
+    print("paraQuestionlist")
+    print(paraQuestionlist)
     return paraQuestionlist
